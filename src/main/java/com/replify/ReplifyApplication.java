58f53c37693891a3751b4a65fcd3e84f74dfd16c
kalinduagathisi <kalinduagathisi@gmail.com>
2025-01-18 19:10:14 +0530

    initial commit

---
+++ src/main/java/com/replify/ReplifyApplication.java
@@ -0,0 +1,13 @@
+package com.replify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/replify/ReplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
