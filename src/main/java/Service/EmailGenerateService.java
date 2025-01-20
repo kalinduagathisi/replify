815edfc92644b3c49da2736a327b52024d96b758
kalinduagathisi <kalinduagathisi@gmail.com>
2025-01-20 09:50:09 +0530

    added generateEmailReply method

--- src/main/java/Service/EmailGenerateService.java
+++ src/main/java/Service/EmailGenerateService.java
@@ -1,7 +1,18 @@
 package Service;
 
+import Dto.EmailRequestDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailGenerateService {
+
+    // generate email reply
+    public String generateEmailReply(EmailRequestDto emailRequest) {
+        // build the prompt
+        //craft a request
+        //do request and get response
+        //return response
+
+        return null;
+    }
 }
