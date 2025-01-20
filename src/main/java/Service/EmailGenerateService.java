914b6a51085d1853b1c2e2a48f82f13286a7988d
kalinduagathisi <kalinduagathisi@gmail.com>
2025-01-20 11:03:23 +0530

    added buildPrompt method

--- src/main/java/Service/EmailGenerateService.java
+++ src/main/java/Service/EmailGenerateService.java
@@ -9,10 +9,21 @@ public class EmailGenerateService {
     // generate email reply
     public String generateEmailReply(EmailRequestDto emailRequest) {
         // build the prompt
+        String prompt = buildPrompt(emailRequest);
         //craft a request
         //do request and get response
         //return response
 
         return null;
+    }
+
+    private String buildPrompt(EmailRequestDto emailRequest) {
+        StringBuilder prompt = new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line.");
+        if (emailRequest.getTone() != null && emailRequest.getTone().isEmpty()) {
+            prompt.append("Use a").append(emailRequest.getTone()).append("tone");
+        }
+        prompt.append("\nOriginal email: \n").append(emailRequest.getEmailContent());
+        return prompt.toString();
     }
 }
