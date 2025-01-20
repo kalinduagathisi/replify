5767275629a3abe65dcf457f717ffbf0907d384a
kalinduagathisi <kalinduagathisi@gmail.com>
2025-01-20 13:09:33 +0530

    implemented extractResponseContent method

--- src/main/java/Service/EmailGenerateService.java
+++ src/main/java/Service/EmailGenerateService.java
@@ -1,6 +1,8 @@
 package Service;
 
 import Dto.EmailRequestDto;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -45,12 +47,23 @@ public class EmailGenerateService {
 
         //return response
         return extractResponseContent(response);
-
-        return null;
     }
 
     private String extractResponseContent(String response) {
-        return null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("contents")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+
+        } catch (Exception e) {
+            return "Error processing request: " + e.getMessage();
+        }
     }
 
     private String buildPrompt(EmailRequestDto emailRequest) {
